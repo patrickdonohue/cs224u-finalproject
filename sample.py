40e2aa77dcb36a01bbbfe7b3a7ff952553ecc49c
patrickdonohue <pdonohue516@gmail.com>
2023-06-09 16:00:52 -0700

    removed exposed api key

--- sample.py
+++ sample.py
@@ -1,20 +1,28 @@
 from googleapiclient import discovery
+import argparse
 import json
 
-API_KEY = 'AIzaSyBkByOUY2Ecr6DTgZVvdeJGF67pV0fij1Q'
 
-client = discovery.build(
-  "commentanalyzer",
-  "v1alpha1",
-  developerKey=API_KEY,
-  discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
-  static_discovery=False,
-)
 
-analyze_request = {
-  'comment': { 'text': 'I hate my friend damon' },
-  'requestedAttributes': {'TOXICITY': {}}
-}
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--api_key", type=str, required=True,
+                        help="API Key for Perspective API, see https://support.perspectiveapi.com/s/docs-get-started")
+  
+  args = parser.parse_args()
+  print(f"Parameters: {args}")
+  client = discovery.build(
+    "commentanalyzer",
+    "v1alpha1",
+    developerKey=args.api_key,
+    discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+    static_discovery=False,
+  )
 
-response = client.comments().analyze(body=analyze_request).execute()
-print(json.dumps(response, indent=2))
+  analyze_request = {
+    'comment': { 'text': 'I hate my friend damon' },
+    'requestedAttributes': {'TOXICITY': {}}
+  }
+
+  response = client.comments().analyze(body=analyze_request).execute()
+  print(json.dumps(response, indent=2))
